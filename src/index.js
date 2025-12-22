d468eaabb0df792bea26dd1df5adfc6826962684
neeraj <neerajpallikonda34@gmail.com>
2025-12-22 16:51:08 +0530

    Connect to Mongodb Atlas

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+
+import connectDB from "./db/index.js";
+
+connectDB();
+
+/*
+import express from "express";
+const app = express()(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log(`Errr: `, error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(`ERROR: `, error);
+    throw err;
+  }
+})();
+*/
